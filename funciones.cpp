b42171b820a8a00813cf6b1ff8ed65be57701690
Enrique-Escobar <a2024111424@estudiantes.upsa.edu.bo>
2024-11-13 13:58:59 -0400

    funciones.cpp

---
+++ funciones.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "funciones.h"  // Incluir el archivo de cabecera
+
+using namespace std;
+
+// Definición de la función saludar
+void saludar() {
+    cout << "¡Hola desde el archivo funciones.cpp!" << endl;
+}
+
+// Definición de la función sumar
+int resta(int a, int b) {
+    return a - b;
+}
+int sumar(int a, int b) {
+    return a + b;
+}
